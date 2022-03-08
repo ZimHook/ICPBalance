8bc60e15bd5232b2276318aaf6880b47f1c062bd
ZimHook <zim764723426@gmail.com>
2022-03-08 18:49:50 +0800

    -

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { principalToAccountAddress, principalToAccountAddressArray, to32bits} from './utils.jsx';
+import { fromHexString, principalToAccountAddressArray} from './utils.jsx';
 import {idlFactory as ledgerIDL} from './candid/ledger.did.js';
 import { Actor, HttpAgent } from "@dfinity/agent";
 import { Button, Input, message } from "antd";
-import { AuthClient } from "@dfinity/auth-client";
+import "./App.css"
 
 const App = () => {
   const [balance, setBalance] = useState(0);
   const [ledger, setLedger] = useState(null);
   const [principal, setPrincipal] = useState("")
+  const [loading, setLoading] = useState(false)
   
   useEffect(async () => {
     let anonymousAgent = new HttpAgent({ ...{ host: 'https://boundary.ic0.app/' } });
@@ -24,17 +25,28 @@ const App = () => {
   }, []);
 
   async function getICPBanlance(){
-
-    let address = principalToAccountAddressArray(principal, 0);
-
+    if(ledger == null){
+      message.info("Please wait for the ledger actor to be created")
+    }
+    setLoading(true)
+    let address
+    if(principal.length == 64){
+      address = fromHexString(principal)
+    } else {
+      try{
+        address = principalToAccountAddressArray(principal, 0);
+      } catch (err) {
+        message.error("Please check your principal/canisterID")
+        setLoading(false)
+        return
+      }
+    }
     var args = {'account' : address};
 
     var icpBalance = await ledger.account_balance(args);
-    console.log(icpBalance);
-    console.log(icpBalance.e8s);
     var numICPBalance = parseInt(icpBalance.e8s);
-    console.log(numICPBalance);
     setBalance(numICPBalance);
+    setLoading(false)
   };
 
   function handleChange(e){
@@ -43,13 +55,14 @@ const App = () => {
 
 
   return (
-    <div>
-      <h2>
-        Principal/CanisterID : <Input onChange={handleChange}></Input>
+    <div className="App">
+      <h2 className="content">
+        Principal/CanisterID/Address : <Input className="input" onChange={handleChange}></Input>
         <br/> 
         ICP balance : {balance/100000000} 
-        <Button type="primary" onClick={getICPBanlance}>
-          query balance
+        <br/> 
+        <Button loading={loading} type="primary" onClick={getICPBanlance}>
+          Query Balance
         </Button>
       </h2>
     </div>
